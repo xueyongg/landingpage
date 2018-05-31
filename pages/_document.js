3b2f6e2ef99f3b0f7e134c2b70d6a5cd6e234d7c
PHUA XUE YONG _ <xyphua.2014@smu.edu.sg>
2018-05-31 12:15:26 +0800

    updated packages

--- pages/_document.js
+++ pages/_document.js
@@ -10,16 +10,6 @@ export default class MyDocument extends Document {
     return (
       <html>
         <Head>
-          {/* <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=UA-119971042-1"
-          />
-          <script>
-            window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(
-              arguments
-            )}
-            gtag('js', new Date()); gtag('config', 'UA-119971042-1');
-          </script> */}
           <style>{`body { margin: 0 } /* custom! */`}</style>
           <meta
             name="viewport"
